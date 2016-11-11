bf8578937d11f55fc7f90e0cd8033fb62f6acbc7
Christian <christianvaldez36@hotmail.com>
2016-11-10 19:32:37 -0800

    Changed prompt from delta to a solid triangle

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,7 +68,7 @@ precmd() {
     print -P '\n%F{51}%~'
 }
 
-export PROMPT='%(?.%F{205}.%F{red})∆%f '
+export PROMPT='%(?.%F{205}.%F{red})▲%f '
 #export PROMPT='%(?.%F{205}.%F{red})⇨%f '
 export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`'
 #export RPROMPT='`git_dirty`'
