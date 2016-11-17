3dff048ab3da6f95dc86921e790a5f7e0331dafa
Christian <christianvaldez36@hotmail.com>
2016-11-17 11:13:20 -0800

    Changed the color of the prompt symbol again

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,7 +68,8 @@ precmd() {
     print -P '\n%F{blue}%~'
 }
 
-export PROMPT='%(?.%F{205}.%F{red})▲%f '
+#export PROMPT='%(?.%F{205}.%F{red})▲%f '
+export PROMPT='%(?.%F{199}.%F{red})▲%f '
 #export PROMPT='%(?.%F{205}.%F{red})⇨%f '
 export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`'
 #export RPROMPT='`git_dirty`'
