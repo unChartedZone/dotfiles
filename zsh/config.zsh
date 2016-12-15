a93811200092dbc081998088f81cdaee346d91f9
Christian Valdez <christianvaldez36@hotmail.com>
2016-12-14 17:34:51 -0800

    Commented out the bottom porition till I figure out how what everything does in this file

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -22,11 +22,11 @@ setopt APPEND_HISTORY
 setopt COMPLETE_ALIASES
 
 # make terminal command navigation sane again
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# bindkey '^[^[[D' backward-word
+# bindkey '^[^[[C' forward-word
+# bindkey '^[[5D' beginning-of-line
+# bindkey '^[[5C' end-of-line
+# bindkey '^[[3~' delete-char
+# bindkey '^[^N' newtab
+# bindkey '^?' backward-delete-char
 
