0f8b112f3f222d7683de2a34817382fab360ad22
Christian Valdez <christianvaldez36@hotmail.com>
2017-01-31 00:24:38 -0800

    Moved content of zshrc to zshrc.symlink and added a zsh file for git that will keep shortcuts

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,5 +1,6 @@
 # git aliases
-# alias ga='git add'
+alias ga='git add'
+alias gs='git status'
 # alias gb='git branch'
 # alias gl='git pull'
 # alias gp='git push'
@@ -18,28 +19,28 @@
 # alias git-count='git shortlog -sn'
 # alias git-undopush="git push -f origin HEAD^:master"
 # alias cpbr="git rev-parse --abbrev-ref HEAD | pbcopy"
-# git root
+# # git root
 # alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
 
 # alias sub-pull='git submodule foreach git pull origin master'
 
 # function give-credit() {
-    # git commit --amend --author $1 <$2> -C HEAD
+#     git commit --amend --author $1 <$2> -C HEAD
 # }
 
-# a simple git rename file function
-# git does not track case-sensitive changes to a filename.
+# # a simple git rename file function
+# # git does not track case-sensitive changes to a filename.
 # function git-rename() {
 #     git mv $1 "${2}-"
 #     git mv "${2}-" $2
 # }
 
-function g() {
-    if [[ $# > 0 ]]; then
-        # if there are arguments, send them to git
-        git $@
-    else
-        # otherwise, run git status
-        git s
-    fi
-}
+# function g() {
+#     if [[ $# > 0 ]]; then
+#         # if there are arguments, send them to git
+#         git $@
+#     else
+#         # otherwise, run git status
+#         git s
+#     fi
+# }
