c08ab5651d764f3c52b641538a5faf77740c4912
Christian Valdez <christianvaldez36@hotmail.com>
2017-02-19 13:53:43 -0800

    Changed plugin theme for hyperterm

--- hyperterm/hyper.js.hardlink.js
+++ hyperterm/hyper.js.hardlink.js
@@ -105,7 +105,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-atom-dark'],
+  plugins: ['hyperyellow'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
