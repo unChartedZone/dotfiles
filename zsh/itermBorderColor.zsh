fd6046fb0db6cf190d9342f76902ddcf22dd63c5
Christian Valdez <christianvaldez36@hotmail.com>
2017-03-03 09:32:38 -0800

    Got rid of some agau green colors, and settled on a yellow shade

--- zsh/itermBorderColor.zsh
+++ zsh/itermBorderColor.zsh
@@ -7,19 +7,10 @@
 # printf -- $'\033]6;1;bg;red;brightness;160\a\033]6;1;bg;green;brightness;64\a\033]6;1;bg;blue;brightness;65\a' #rgb(160, 64, 65)
 
 # This is a nice shade of yellow I like
-# printf -- $'\033]6;1;bg;red;brightness;222\a\033]6;1;bg;green;brightness;147\a\033]6;1;bg;blue;brightness;95\a' #rgb(222, 147, 95)
+printf -- $'\033]6;1;bg;red;brightness;222\a\033]6;1;bg;green;brightness;147\a\033]6;1;bg;blue;brightness;95\a' #rgb(222, 147, 95)
 
 # Shade of purple I like
-printf -- $'\033]6;1;bg;red;brightness;169\a\033]6;1;bg;green;brightness;53\a\033]6;1;bg;blue;brightness;227\a' #rgb(169, 53, 227)
-
-# This a shade of light agua green I like
-
-# printf -- $'\033]6;1;bg;red;brightness;14\a\033]6;1;bg;green;brightness;201\a\033]6;1;bg;blue;brightness;211\a' #rgb(14, 201, 211)
-# printf -- $'\033]6;1;bg;red;brightness;81\a\033]6;1;bg;green;brightness;190\a\033]6;1;bg;blue;brightness;149\a' #rgb(81, 190, 149)
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;255\a\033]6;1;bg;blue;brightness;204\a' #rgb(0, 255, 204)
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;153\a\033]6;1;bg;blue;brightness;153\a' #rgb(0, 153, 153)
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;128\a\033]6;1;bg;blue;brightness;128\a' #rgb(0, 128, 128)
-# printf -- $'\033]6;1;bg;red;brightness;28\a\033]6;1;bg;green;brightness;211\a\033]6;1;bg;blue;brightness;155\a' #rgb(28, 211, 155)
+# printf -- $'\033]6;1;bg;red;brightness;169\a\033]6;1;bg;green;brightness;53\a\033]6;1;bg;blue;brightness;227\a' #rgb(169, 53, 227)
 
 # This is a shade of dark green I like
 # printf -- $'\033]6;1;bg;red;brightness;12\a\033]6;1;bg;green;brightness;75\a\033]6;1;bg;blue;brightness;51\a' #rgb(12, 75, 51)
