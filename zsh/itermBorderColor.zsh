4cf3e879da59f14f5268c6011a3ec43ef87d9843
Christian Valdez <christianvaldez36@hotmail.com>
2017-03-03 01:23:54 -0800

    Organized the file better

--- zsh/itermBorderColor.zsh
+++ zsh/itermBorderColor.zsh
@@ -1,37 +1,30 @@
 # Should set the tab bar for iterm
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;0\a\033]6;1;bg;blue;brightness;0\a'
-# printf -- $'\033]6;1;bg;red;brightness;29\a\033]6;1;bg;green;brightness;33\a\033]6;1;bg;blue;brightness;31\a'
-# printf -- $'\033]6;1;bg;red;brightness;66\a\033]6;1;bg;green;brightness;244\a\033]6;1;bg;blue;brightness;176\a'
+# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;0\a\033]6;1;bg;blue;brightness;0\a' #rgb(0, 0, 0)
+# printf -- $'\033]6;1;bg;red;brightness;29\a\033]6;1;bg;green;brightness;33\a\033]6;1;bg;blue;brightness;31\a' #rgb(29, 33, 31)
+# printf -- $'\033]6;1;bg;red;brightness;66\a\033]6;1;bg;green;brightness;244\a\033]6;1;bg;blue;brightness;176\a' #rgb(66, 244, 176)
 
 # This is a nice shade of red I like
-printf -- $'\033]6;1;bg;red;brightness;160\a\033]6;1;bg;green;brightness;64\a\033]6;1;bg;blue;brightness;65\a'
+# printf -- $'\033]6;1;bg;red;brightness;160\a\033]6;1;bg;green;brightness;64\a\033]6;1;bg;blue;brightness;65\a' #rgb(160, 64, 65)
 
 # This is a nice shade of yellow I like
-# printf -- $'\033]6;1;bg;red;brightness;222\a\033]6;1;bg;green;brightness;147\a\033]6;1;bg;blue;brightness;95\a'
+# printf -- $'\033]6;1;bg;red;brightness;222\a\033]6;1;bg;green;brightness;147\a\033]6;1;bg;blue;brightness;95\a' #rgb(222, 147, 95)
 
-# This is a nice shade of purple I like
-# printf -- $'\033]6;1;bg;red;brightness;215\a\033]6;1;bg;green;brightness;134\a\033]6;1;bg;blue;brightness;230\a'
-# Another shade of purple I like
-printf -- $'\033]6;1;bg;red;brightness;215\a\033]6;1;bg;green;brightness;160\a\033]6;1;bg;blue;brightness;242\a' #rgb(215, 160, 242)
+# Shades of purple I like
+# printf -- $'\033]6;1;bg;red;brightness;215\a\033]6;1;bg;green;brightness;134\a\033]6;1;bg;blue;brightness;230\a' #rgb(215, 134, 230)
+# printf -- $'\033]6;1;bg;red;brightness;215\a\033]6;1;bg;green;brightness;160\a\033]6;1;bg;blue;brightness;242\a' #rgb(215, 160, 242)
 printf -- $'\033]6;1;bg;red;brightness;169\a\033]6;1;bg;green;brightness;53\a\033]6;1;bg;blue;brightness;227\a' #rgb(169, 53, 227)
 
 # This a shade of light agua green I like
-# rgb(81, 190, 149)
-# rgb(14, 201, 211)
-# rgb(28, 211, 155)
-# rgb(0, 255, 204)
-# rgb(0, 153, 153)
-# rgb(0, 128, 128)
-# printf -- $'\033]6;1;bg;red;brightness;81\a\033]6;1;bg;green;brightness;190\a\033]6;1;bg;blue;brightness;149\a'
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;255\a\033]6;1;bg;blue;brightness;204\a'
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;153\a\033]6;1;bg;blue;brightness;153\a'
-# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;128\a\033]6;1;bg;blue;brightness;128\a'
-# printf -- $'\033]6;1;bg;red;brightness;28\a\033]6;1;bg;green;brightness;211\a\033]6;1;bg;blue;brightness;155\a'
+
+# printf -- $'\033]6;1;bg;red;brightness;14\a\033]6;1;bg;green;brightness;201\a\033]6;1;bg;blue;brightness;211\a' #rgb(14, 201, 211)
+# printf -- $'\033]6;1;bg;red;brightness;81\a\033]6;1;bg;green;brightness;190\a\033]6;1;bg;blue;brightness;149\a' #rgb(81, 190, 149)
+# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;255\a\033]6;1;bg;blue;brightness;204\a' #rgb(0, 255, 204)
+# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;153\a\033]6;1;bg;blue;brightness;153\a' #rgb(0, 153, 153)
+# printf -- $'\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;128\a\033]6;1;bg;blue;brightness;128\a' #rgb(0, 128, 128)
+# printf -- $'\033]6;1;bg;red;brightness;28\a\033]6;1;bg;green;brightness;211\a\033]6;1;bg;blue;brightness;155\a' #rgb(28, 211, 155)
 
 # This is a shade of dark green I like
-#rgb(12, 75, 51)
-# printf -- $'\033]6;1;bg;red;brightness;12\a\033]6;1;bg;green;brightness;75\a\033]6;1;bg;blue;brightness;51\a'
-#
+# printf -- $'\033]6;1;bg;red;brightness;12\a\033]6;1;bg;green;brightness;75\a\033]6;1;bg;blue;brightness;51\a' #rgb(12, 75, 51)
+
 # Shade of salmon pink
-# rgb(237, 101, 104)
-# printf -- $'\033]6;1;bg;red;brightness;237\a\033]6;1;bg;green;brightness;101\a\033]6;1;bg;blue;brightness;104\a'
+# printf -- $'\033]6;1;bg;red;brightness;237\a\033]6;1;bg;green;brightness;101\a\033]6;1;bg;blue;brightness;104\a' # rgb(237, 101, 104)
