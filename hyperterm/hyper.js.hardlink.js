62db2417dfd76de3330acd1b20818b4626ce54f7
Christian Valdez <christianvaldez36@hotmail.com>
2017-03-17 10:56:45 -0700

    Added nerd font support for hyperterm

--- hyperterm/hyper.js.hardlink.js
+++ hyperterm/hyper.js.hardlink.js
@@ -5,7 +5,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Monaco, Fira Mono for Powerline,  Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Monaco, FuraCode Nerd Font,  Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     //cursorColor: 'rgba(248,28,229,0.8)',
