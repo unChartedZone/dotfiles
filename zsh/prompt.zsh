86c75f40f627f0dcee0d6e7782b789957739963c
Christian Valdez <christianvaldez36@hotmail.com>
2017-03-21 11:45:06 -0700

    Added a function that alerts me of normal mode but messes up git rprompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -50,6 +50,18 @@ git_arrows() {
     echo $arrows 
 }
 
+# Enable vi mode
+bindkey -v
+
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]% %{$reset_color%}"
+	RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $(git_custom_status) $EPS1"
+	zle reset-prompt
+}
+
+# zle -N zle-line-init
+# zle -N zle-keymap-select
+
 precmd() {
     vcs_info
     print -P '\n%F{blue}%~'
