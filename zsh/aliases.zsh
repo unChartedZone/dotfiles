fcbb3119cc5f0db81f24d18545c4c75c117f2b18
Christian Valdez <christianvaldez36@hotmail.com>
2017-08-30 11:03:52 -0700

    fixed school aliase

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias alucard="ssh chris@alucard.io"
 alias unchartedzone="ssh chris@unchartedzone.me"
 alias wordpress="ssh chris@138.68.246.123"
 alias bro="tmux -f ~/.dotfiles/tmux/bro.tmux.conf.symlink attach"
-alias school="cd ~/Documents/chrisSchoolWork/"
+alias school="cd ~/Documents/chrisSchoolWork/fall2017"
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
