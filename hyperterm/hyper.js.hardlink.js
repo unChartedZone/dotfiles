f01a1ceb2a122e6279ed9ef3c3a39e598cb92489
Christian Valdez <christianvaldez36@hotmail.com>
2017-09-21 19:16:49 -0700

    Added hyperborder plugin to hyperterm

--- hyperterm/hyper.js.hardlink.js
+++ hyperterm/hyper.js.hardlink.js
@@ -105,10 +105,13 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-hybrid'],
+  plugins: ['hyperterm-hybrid', 'hyperborder'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
+	hyperBorder: {
+		borderWidth: '1px'
+	}
 };
