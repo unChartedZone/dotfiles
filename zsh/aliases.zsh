25b3073a3de9c5a30eed02174a869b0caef9cff2
Christian Valdez <christianvaldez36@hotmail.com>
2017-10-09 18:14:43 -0700

    added aliases to ssh into my manjaro virtual machine and meteor_test server

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,8 +8,10 @@ alias wordpress="ssh chris@138.68.246.123"
 alias bro="tmux -f ~/.dotfiles/tmux/bro.tmux.conf.symlink attach"
 alias school="cd ~/Documents/chrisSchoolWork/fall2017"
 alias rscript="Rscript"
-alias pie="ssh chris@192.168.1.84"
-alias ice="ssh chris@107.170.209.236"
+alias pie="ssh chris@192.168.1.84" # Shortcut to ssh into my raspberry pie
+alias manjaro="ssh chris@146.244.240.73"
+alias ice="ssh chris@107.170.209.236" #Server used for CS532 project
+alias meteor_test_server="ssh chris@162.243.128.147"
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
