a00fe8951a3ea01d4d5a8530b8aba1c8b7f651bb
Christian Valdez <christianvaldez36@hotmail.com>
2020-08-09 22:02:44 -0700

    added zsh theme and some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,11 +16,24 @@ alias tat='tmux attach -t'
 alias tns='tmux new-session -s'
 alias tks="tmux kill-session"
 
+# alias to run tmux profile script
+alias bro="tmux -f ~/.dotfiles/tmux/bro.tmux.conf.symlink attach"
+
 # Filesystem aliases
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias dot="cd ~/.dotfiles"
+alias dev="cd ~/Workshop"
 alias l="ls -l"
 alias la="ls -la"
+
+# git aliases
+alias ga='git add'
+alias gb='git branch'
+alias gdiff='git diff'
+alias glog="git log"
+alias gpull='git pull'
+alias gpush='git push'
+alias gs='git status'
