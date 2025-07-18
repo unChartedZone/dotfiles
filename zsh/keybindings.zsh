4e0a3c049a3bf77eae4d5020288f90df3df1f705
Christian Valdez <christianvaldez36@hotmail.com>
2025-07-17 19:17:43 -0700

    added some keybindings

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -2,3 +2,8 @@
 bindkey '^f' autosuggest-accept
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
+
+# maybe keep
+# bindkey "^P" up-line-or-search
+# bindkey "^N" down-line-or-search
+
