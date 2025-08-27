def2f9ba7dd3ec274a47db19a6ae63705681631d
Christian Valdez <christianvaldez36@hotmail.com>
2025-08-27 14:26:18 -0700

    adds some nvim plugins
    - still need to configure blamer plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,60 +1,99 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = function()
-      local conf = require "nvchad.configs.telescope"
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = function()
+			local conf = require("nvchad.configs.telescope")
 
-       conf.defaults.file_ignore_patterns = { "node_modules" }
-       conf.defaults.mappings.i = {
-         ["<C-j>"] = require("telescope.actions").move_selection_next,
-         ["<C-k>"] = require("telescope.actions").move_selection_previous,
-         ["<Esc>"] = require("telescope.actions").close,
-       }
-       return conf
-    end,
-  },
- {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "scss",
-        "javascript",
-        "styled",
-        "typescript",
-        "tsx",
-        "elixir",
-      },
-    },
-  }
+			conf.defaults.file_ignore_patterns = { "node_modules" }
+			conf.defaults.mappings.i = {
+				["<C-j>"] = require("telescope.actions").move_selection_next,
+				["<C-k>"] = require("telescope.actions").move_selection_previous,
+				["<Esc>"] = require("telescope.actions").close,
+			}
+			return conf
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"scss",
+				"javascript",
+				"styled",
+				"typescript",
+				"tsx",
+				"elixir",
+			},
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = false },
+			indent = { enabled = false },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+	},
+	{
+		"APZelos/blamer.nvim",
+		config = function()
+			vim.g.blamer_enabled = true
+		end,
+	},
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
