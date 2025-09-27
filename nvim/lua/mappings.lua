1c6f14080e6596a6579540656a9a8487d5fe6985
Chris Valdez <christian@lawmatics.com>
2025-09-26 22:28:22 -0700

    add mappings to search for symbols in a document and to trigger formatting with conform

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -8,15 +8,23 @@ map("i", "jk", "<ESC>")
 
 -- Toggle Float Term
 map({ "n", "t" }, "<leader>tl", function()
-  require("nvchad.term").toggle { pos = "vsp", id = "floo", size = 0.3 }
+	require("nvchad.term").toggle({ pos = "vsp", id = "floo", size = 0.3 })
 end, { noremap = true, desc = "Remap toggle side term" })
 
 map({ "n", "t" }, "<C-t>", function()
-  require("nvchad.term").toggle { pos = "float", id = "floaty" }
+	require("nvchad.term").toggle({ pos = "float", id = "floaty" })
 end, { noremap = true, desc = "Toggle floating terminal" })
 
 map("n", "<leader>gb", function()
-  require("snacks").git.blame_line()
+	require("snacks").git.blame_line()
 end, { desc = "Git Blame Line" })
+
+map("n", "<leader>fr", function()
+	require("telescope.builtin").lsp_document_symbols()
+end, { desc = "Find document symbols" })
+
+map("n", "<leader>fm", function()
+	require("conform").format()
+end, { desc = "Format file" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
