0269cc34616fba9edff43efa02323981cf43761b
Chris Valdez <christian@lawmatics.com>
2025-10-19 23:49:11 -0700

    updated bin folders to be on path

--- zsh/path.sh
+++ zsh/path.sh
@@ -5,3 +5,15 @@ export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
 
 # Added by Windsurf
 export PATH="/Users/cvaldez/.codeium/windsurf/bin:$PATH"
+
+# check for custom bin directory and add to path
+if [[ -d ~/bin ]]; then
+    export PATH=~/bin:$PATH
+fi
+
+if [[ -d ~/config/emacs/bin ]]; then
+    export PATH=~/.config/emacs/bin:$PATH
+fi
+
+# adding path directory for custom scripts
+export PATH=$DOTFILES/bin:$PATH
