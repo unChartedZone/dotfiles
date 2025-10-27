44f7ee78689d3e6f15fba96eb2e2d397658cd197
Chris Valdez <christian@lawmatics.com>
2025-10-26 23:53:32 -0700

    refactored zsh configs to be more clean

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -13,15 +13,6 @@
 # ██║     ███████╗╚██████╔╝╚██████╔╝██║██║ ╚████║███████║
 # ╚═╝     ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝╚══════╝
 #
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
@@ -46,8 +37,8 @@ zinit light-mode for \
 
 ### End of Zinit's installer chunk
 
-# Powerlevel10k
-zinit ice depth=1; zinit light romkatv/powerlevel10k
+# Starship
+eval "$(starship init zsh)"
 # Syntax Highlighting
 zinit light zsh-users/zsh-syntax-highlighting
 # Completions
@@ -56,3 +47,15 @@ zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 # FZF tab
 zinit light Aloxaf/fzf-tab
+
+# FZF
+eval "$(fzf --zsh)"
+
+# Rbenv
+eval "$(rbenv init - --no-rehash zsh)"
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
