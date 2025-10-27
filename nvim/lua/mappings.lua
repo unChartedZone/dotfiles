cf67da88a03f6874dc2c01651fe0cd67ca69eac5
Chris Valdez <christian@lawmatics.com>
2025-10-26 23:56:05 -0700

    add more mappings in neovim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,13 +7,13 @@ local map = vim.keymap.set
 map("i", "jk", "<ESC>")
 
 -- Toggle Float Term
-map({ "n", "t" }, "<leader>tl", function()
-	require("nvchad.term").toggle({ pos = "vsp", id = "floo", size = 0.3 })
-end, { noremap = true, desc = "Remap toggle side term" })
+map({ "n", "t" }, "<leader>tf", function()
+	require("nvchad.term").toggle({ pos = "float", id = "float mcfloatface", size = 0.3 })
+end, { desc = "Remap floating term" })
 
-map({ "n", "t" }, "<C-t>", function()
-	require("nvchad.term").toggle({ pos = "float", id = "floaty" })
-end, { noremap = true, desc = "Toggle floating terminal" })
+map({ "n", "t" }, "<M-`>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floaty", size = 0.3 }
+end, { noremap = true, desc = "Remap toggle side term" })
 
 map("n", "<leader>gb", function()
 	require("snacks").git.blame_line()
@@ -26,5 +26,13 @@ end, { desc = "Find document symbols" })
 map("n", "<leader>fm", function()
 	require("conform").format()
 end, { desc = "Format file" })
+
+map("n", "<leader>ca", function()
+  vim.lsp.buf.code_action()
+end, { desc = "Code actions" })
+
+map("n", "<leader>ls", function()
+  vim.lsp.buf.signature_help { focus = false, silent = true, max_height = 7, border = "single" }
+end, { desc = "LSP signature help" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
