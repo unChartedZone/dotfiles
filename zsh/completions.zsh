16b94ab5de47c0cd114f4a9c063126fc0e5cb1be
Christian Valdez <christianvaldez36@hotmail.com>
2026-04-10 18:39:47 -0700

    tweaked zsh completions

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -7,3 +7,4 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:ls:*' fzf-preview 'ls --color $realpath'
