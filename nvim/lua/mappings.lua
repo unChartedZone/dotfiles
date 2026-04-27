427cbe94b895ae3150cb08737ff2d8e151d0d634
Chris Valdez <christian@lawmatics.com>
2026-04-27 13:43:35 -0700

    add mapping to open document references in telescope pane

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,9 +4,10 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "gO", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "LSP document symbols (Telescope)" })
+map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { desc = "LSP go to definition (Telescope)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
