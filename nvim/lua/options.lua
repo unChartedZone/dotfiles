8974ebdf4bd11fa64608d14127c7120e85b37ec9
Chris Valdez <christian@lawmatics.com>
2026-04-27 13:42:03 -0700

    add a couple of options to nvim
    - set wrap false
    - enable cursorline
    - fill empty chars with '/'
    - enable wrap for markdown files

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,4 +3,18 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
+o.wrap = false -- disabled line wrap
+
+-- For diffview.nvim
+local set = vim.opt -- set options
+set.fillchars:append { diff = "╱" }
+
+-- Set wrap for markdown files 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true -- Optional: wraps at words instead of characters
+  end,
+})
