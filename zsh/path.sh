c90ba124f9c6d4ac9afdcc55c562037aa39947b7
Chris Valdez <christian@lawmatics.com>
2026-09-04 15:39:12 -0700

    updated some zsh path configs

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,19 +1,29 @@
 # Homebrew
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
-export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
+export PATH="/opt/homebrew/opt/postgresql@17/bin:$PATH"
 
 # Added by Windsurf
 export PATH="/Users/cvaldez/.codeium/windsurf/bin:$PATH"
 
+# Added by Antigravity IDE
+export PATH="/Users/cvaldez/.antigravity-ide/antigravity-ide/bin:$PATH"
+
+# Added by Antigravity CLI installer
+export PATH="/Users/cvaldez/.local/bin:$PATH"
+
+export PATH="$HOME/.local/bin:$PATH"
 # check for custom bin directory and add to path
 if [[ -d ~/bin ]]; then
     export PATH=~/bin:$PATH
 fi
 
-if [[ -d ~/config/emacs/bin ]]; then
+if [[ -d ~/.config/emacs/bin ]]; then
     export PATH=~/.config/emacs/bin:$PATH
 fi
 
 # adding path directory for custom scripts
 export PATH=$DOTFILES/bin:$PATH
+
+# PHP Composer
+export PATH="$HOME/.config/composer/vendor/bin:$PATH"
