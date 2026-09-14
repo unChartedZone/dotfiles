a35909f078250fe332f32bc9131020aac67c74c9
Chris Valdez <christian@lawmatics.com>
2026-09-13 20:07:02 -0700

    switch conform to use biome for js/ts, also enable format on save

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -3,18 +3,19 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    javascript = { "eslint_d", "prettier" },
-    javascriptreact = { "eslint_d", "prettier" },
-    typescript = { "eslint_d", "prettier" },
-    typescriptreact = { "eslint_d", "prettier" },
-		ruby = { "rubocop" },
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
+    json = { "biome" },
+    ruby = { "rubocop" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
